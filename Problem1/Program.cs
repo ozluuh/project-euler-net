bb446da9ec016b7ace1fb939752eb97575158ca8
ozluuh <ozluuh@gmail.com>
2021-03-02 15:49:36 -0300

    solved problem 1

---
+++ Problem1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumOfMultiples = 0;
+
+            for (int counter = 1; counter < 1000; counter++)
+            {
+                if (counter % 3 == 0 || counter % 5 == 0)
+                {
+                    sumOfMultiples += counter;
+                }
+            }
+
+            Console.WriteLine($"The sum of multiples is {sumOfMultiples}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Repo destinated to solve problems of [Project Euler](https://projecteuler.net/ab
 ## Purpose
 
 improve logical reasoning and more efficiently solve mathematical and logical problems.
+
+## Menu
+
+- [Problem 1](#problem-1)
+
+### Problem 1
+
+#### Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
